22756180c707f609b9e6edb18940b844e86b5b0f
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-05-09 13:31:02 +0200

    ECP-1 Add FormatException

---
+++ src/Exception/FormatException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Exception;
+
+use Exception;
+
+/**
+ * Specifies a formatting problem.
+ */
+class FormatException extends Exception
+{
+}
