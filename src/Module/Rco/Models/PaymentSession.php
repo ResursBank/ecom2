dce0a7edd646770f1118a3a0458efc55d7c0d044
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-09 16:23:15 +0200

    ECP-5 add PaymentSession model

--- src/Module/Rco/Models/PaymentSession.php
+++ src/Module/Rco/Models/PaymentSession.php
@@ -2,3 +2,17 @@
 
 declare(strict_types=1);
 
+namespace Resursbank\Ecom\Module\Rco\Models;
+
+/**
+ * Defines a payment session
+ */
+class PaymentSession
+{
+    public string $paymentSessionId;
+    public string $iframe;
+    public string $script;
+    public Customer $customer;
+    public string $baseUrl;
+    public string $html;
+}
