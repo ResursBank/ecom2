0e25ba2ef1d1c3b8b1add7e29eb49aefced9ad21
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-14 10:17:27 +0200

    ECP-5 Add type determination and docstrings

--- src/Lib/Collection/Collection.php
+++ src/Lib/Collection/Collection.php
@@ -11,10 +11,13 @@ use Resursbank\Ecom\Exception\TypeException;
 
 /**
  * Base collection class
+ *
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
  */
 class Collection implements ArrayAccess, Iterator, Countable
 {
     private const TYPE_ERR = "Collection requires data to be of type %s, received %s";
+    private const TYPE_ERR_NO_DATA = "No type or data specified";
     // @todo: Enforce type when adding data, need to drop this again for presta stuff...
 
     protected string $type;
@@ -28,10 +31,32 @@ class Collection implements ArrayAccess, Iterator, Countable
      */
     public function __construct(array $data, string $type = null)
     {
+        $type = $this->determineType($data, $type);
         $this->verifyDataArrayType($data, $type);
         $this->data = $data;
         $this->type = $type;
         $this->position = 0;
+    }
+
+    /**
+     * Get collection from specified type or first element of data array
+     *
+     * @param array $data
+     * @param string|null $type
+     * @return string
+     * @throws TypeException
+     */
+    private function determineType(array $data, string $type = null): string
+    {
+        if ($type) {
+            return $type;
+        }
+
+        if (!empty($data) && isset($data[0])) {
+            return is_object($data[0]) ? $data[0]::class : gettype($data[0]);
+        }
+
+        throw new TypeException(message: self::TYPE_ERR_NO_DATA);
     }
 
     /**
@@ -50,7 +75,7 @@ class Collection implements ArrayAccess, Iterator, Countable
                 (!is_object($item) && gettype($item) !== $type)
             ) {
                 throw new TypeException(
-                    sprintf(
+                    message: sprintf(
                         self::TYPE_ERR,
                         $type,
                         (is_object($item) ? $item::class : gettype($item))
@@ -60,16 +85,29 @@ class Collection implements ArrayAccess, Iterator, Countable
         }
     }
 
+    /**
+     * @inheritDoc
+     */
     public function count(): int
     {
         return count($this->data);
     }
 
-    public function getType(): ?string
+    /**
+     * Get collection type
+     *
+     * @return string
+     */
+    public function getType(): string
     {
         return $this->type;
     }
 
+    /**
+     * Get data array from collection
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return $this->data;
@@ -94,16 +132,25 @@ class Collection implements ArrayAccess, Iterator, Countable
         }
     }
 
+    /**
+     * @inheritDoc
+     */
     public function offsetExists($offset): bool
     {
         return isset($this->data[$offset]);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function offsetUnset($offset): void
     {
         unset($this->data[$offset]);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function offsetGet($offset): mixed
     {
         if (!isset($this->data[$offset])) {
@@ -113,26 +160,41 @@ class Collection implements ArrayAccess, Iterator, Countable
         return $this->data[$offset];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function rewind(): void
     {
         $this->position = 0;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function current(): mixed
     {
         return $this->data[$this->position];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function key(): mixed
     {
         return $this->position;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function next(): void
     {
         ++$this->position;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function valid(): bool
     {
         return isset($this->data[$this->position]);
