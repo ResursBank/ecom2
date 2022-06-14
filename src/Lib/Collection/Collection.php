59c2003ef6d491fdf59465c67aa7c7a3bb537a71
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-14 09:53:28 +0200

    ECP-5 Add data type verification to Collection

--- src/Lib/Collection/Collection.php
+++ src/Lib/Collection/Collection.php
@@ -7,20 +7,57 @@ namespace Resursbank\Ecom\Lib\Collection;
 use ArrayAccess;
 use Iterator;
 use Countable;
+use Resursbank\Ecom\Exception\TypeException;
 
 /**
  * Base collection class
  */
 class Collection implements ArrayAccess, Iterator, Countable
 {
-    protected ?string $type = null;
+    private const TYPE_ERR = "Collection requires data to be of type %s, received %s";
+    // @todo: Enforce type when adding data, need to drop this again for presta stuff...
+
+    protected string $type;
     private array $data;
     private int $position;
 
-    public function __construct(array $data)
+    /**
+     * @param array $data
+     * @param string|null $type
+     * @throws TypeException
+     */
+    public function __construct(array $data, string $type = null)
     {
+        $this->verifyDataArrayType($data, $type);
         $this->data = $data;
+        $this->type = $type;
         $this->position = 0;
+    }
+
+    /**
+     * Verify the type of objects in collection data
+     *
+     * @param array $data
+     * @param string $type
+     * @return void
+     * @throws TypeException
+     */
+    private function verifyDataArrayType(array $data, string $type): void
+    {
+        foreach ($data as $item) {
+            if (
+                (is_object($item) && $item::class !== $type) ||
+                (!is_object($item) && gettype($item) !== $type)
+            ) {
+                throw new TypeException(
+                    sprintf(
+                        self::TYPE_ERR,
+                        $type,
+                        (is_object($item) ? $item::class : gettype($item))
+                    )
+                );
+            }
+        }
     }
 
     public function count(): int
@@ -38,8 +75,18 @@ class Collection implements ArrayAccess, Iterator, Countable
         return $this->data;
     }
 
-    public function offsetSet($offset, $value): void
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
+     * @throws TypeException
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
     {
+        if (!($value instanceof $this->type)) {
+            throw new TypeException(sprintf(self::TYPE_ERR, $this->type, $value::class));
+        }
+
         if (is_null($offset)) {
             $this->data[] = $value;
         } else {
