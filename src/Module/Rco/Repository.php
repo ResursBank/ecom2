93a02d99785041f391f997011ba31604be5b4c71
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-15 13:07:58 +0200

    ECP-5 Add method stub for updatePayment

--- src/Module/Rco/Repository.php
+++ src/Module/Rco/Repository.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Resursbank\Ecom\Module\Rco;
 
 use Resursbank\Ecom\Module\Module as CoreModule;
-use Resursbank\Ecom\Module\Rco\Models\PaymentRequest\PaymentRequest;
+use Resursbank\Ecom\Module\Rco\Models\PaymentRequest;
 
 class Repository extends CoreModule
 {
-    public static function initializePayment(
-        PaymentRequest $request
-    ):void  {
+    public static function initPayment(
+        PaymentRequest\Request $request
+    ):PaymentRequest\Response  {
+        $response = PaymentRequest\Request::create(request: $request);
+
+        return $response;
     }
+
+    public static function updatePayment(
+        PaymentRequest\Request
+    )
 }
