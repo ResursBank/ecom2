90fb31efeecc4516f1adc76631b2f32572949af9
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-20 15:12:04 +0200

    ECP-5 Add Request class for UpdatePayment

---
+++ src/Module/Rco/Models/UpdatePayment/Request.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Module\Rco\Models\UpdatePayment;
+
+use Resursbank\Ecom\Lib\Model\Model;
+use Resursbank\Ecom\Module\Rco\Models\MetaDataCollection;
+
+/**
+ * Defines a payment request object
+ * @SuppressWarnings(PHPMD.LongVariable)
+ */
+class Request extends Model
+{
+    public function __construct(
+        public OrderLineCollection $orderLines,
+        public Customer $customer,
+        public string $successUrl,
+        public string $backUrl,
+        public string $shopUrl,
+        public ?string $paymentCreatedCallbackUrl = null,
+        public ?MetaDataCollection $metaData = null
+    ) {
+        parent::__construct();
+    }
+}
