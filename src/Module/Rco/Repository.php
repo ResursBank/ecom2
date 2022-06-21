444478cfe86d79911ed1c35efc74c090626ed79a
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-06-21 10:38:03 +0200

    ECP-5 Add method stubs for Repository class

--- src/Module/Rco/Repository.php
+++ src/Module/Rco/Repository.php
@@ -9,43 +9,72 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\Rco;
 
+use ReflectionException;
 use Resursbank\Ecom\Module\Module as CoreModule;
 use Resursbank\Ecom\Module\Rco\Api\InitPayment;
+use Resursbank\Ecom\Module\Rco\Api\UpdatePayment;
 use Resursbank\Ecom\Module\Rco\Models\InitPayment\Request as InitPaymentRequest;
 use Resursbank\Ecom\Module\Rco\Models\InitPayment\Response as InitPaymentResponse;
+use Resursbank\Ecom\Module\Rco\Models\UpdatePayment\Request as UpdatePaymentRequest;
+use Resursbank\Ecom\Module\Rco\Models\UpdatePayment\Response as UpdatePaymentResponse;
 
 /**
  * Main entrypoint for interfacing with the RCO API programmatically
  */
 class Repository extends CoreModule
 {
+    public const HOSTNAME_PROD = 'checkout.resurs.com';
+    public const HOSTNAME_TEST = 'omnitest.resurs.com';
+
     /**
      * Initialize a payment session
      *
      * @param InitPaymentRequest $request
      * @param string $orderReference
      * @return InitPaymentResponse
-     * @throws \ReflectionException
+     * @throws ReflectionException
      */
-    public function initPayment(
-        InitPaymentRequest $request,
-        string $orderReference
-    ): InitPaymentResponse  {
-        return InitPayment::call(request: $request, orderReference: $orderReference);
+    public function initPayment(InitPaymentRequest $request, string $orderReference): InitPaymentResponse
+    {
+        return (new InitPayment(config: $this->config))
+            ->call(request: $request, orderReference: $orderReference);
     }
 
     /**
      * Update an existing payment session
      *
-     * @param UpdatePayment\Request $request
-     * @return void
+     * @param UpdatePaymentRequest $request
+     * @param string $orderReference
+     * @return UpdatePaymentResponse
+     * @throws ReflectionException
      */
-    public function updatePayment(
-        UpdatePayment\Request $request
-    ): void {
+    public function updatePayment(UpdatePaymentRequest $request, string $orderReference): UpdatePaymentResponse
+    {
+        return (new UpdatePayment(config: $this->config))
+            ->call(request: $request, orderReference: $orderReference);
     }
 
     public function updatePaymentReference(): void
+    {
+    }
+
+    public function getPayment(string $orderReference): void
+    {
+    }
+
+    public function registerCallback(string $callback, string $uriTemplate): void
+    {
+    }
+
+    public function getCallbacks(): void
+    {
+    }
+
+    public function getCallback(string $callback): void
+    {
+    }
+
+    public function unregisterCallback(string $callback): void
     {
     }
 }
