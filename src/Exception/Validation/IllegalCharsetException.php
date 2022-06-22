305ebc52c6bba576d54d2571253b3aad4fd748e1
Peter Sjöqvist <info@mageparts.com>
2022-06-22 11:41:28 +0200

    ECP-27
    * Added Exception classes.

---
+++ src/Exception/Validation/IllegalCharsetException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Exception\Validation;
+
+use Exception;
+use Resursbank\Ecom\Exception\ValidationException;
+
+/**
+ * String contains illegal characters.
+ */
+class IllegalCharsetException extends ValidationException
+{
+}
