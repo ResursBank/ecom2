fdff870520e33c3b8a39bae689a905e661dcc2fd
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-07-11 16:25:04 +0200

    ECP-31 Add simple Jwt auth test

---
+++ tests/Integration/Lib/Network/Model/Auth/JwtTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Resursbank\EcomTest\Integration\Lib\Network\Model\Auth;
+
+use PHPUnit\Framework\TestCase;
+use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Lib\Log\FileLogger;
+use Resursbank\Ecom\Lib\Network\Model\Auth\Jwt;
+
+class JwtTest extends TestCase
+{
+    public function testGetToken(): void
+    {
+        Config::setup(
+            logger: $this->createMock(originalClassName: FileLogger::class),
+            jwtAuth: new Jwt(
+                clientId: $_ENV['JWT_AUTH_CLIENT_ID'],
+                clientSecret: $_ENV['JWT_AUTH_CLIENT_SECRET'],
+                scope: $_ENV['JWT_AUTH_SCOPE'],
+                grantType: $_ENV['JWT_AUTH_GRANT_TYPE']
+            )
+        );
+
+        $token = Config::$instance->jwtAuth->getToken();
+        $currentTime = time();
+
+        $this->assertEquals(
+            expected: 'Bearer',
+            actual: $token->tokenType
+        );
+        $this->assertGreaterThan(
+            expected: $currentTime,
+            actual: $token->validUntil
+        );
+    }
+}
