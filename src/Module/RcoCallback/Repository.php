c22809461ac5216cc297bd4617c32df4495bbf9b
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-07-11 08:29:40 +0200

    ECP-35 Add docblocks to RcoCallback::Repository

--- src/Module/RcoCallback/Repository.php
+++ src/Module/RcoCallback/Repository.php
@@ -9,7 +9,12 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\RcoCallback;
 
+use JsonException;
 use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Exception\CurlException;
+use Resursbank\Ecom\Exception\Validation\EmptyValueException;
+use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
+use Resursbank\Ecom\Exception\ValidationException;
 use Resursbank\Ecom\Module\Module as CoreModule;
 use Resursbank\Ecom\Module\RcoCallback\Models\Callback;
 use Resursbank\Ecom\Module\RcoCallback\Models\CallbackCollection;
@@ -27,24 +32,51 @@ class Repository extends CoreModule
     public const HOSTNAME_PROD = 'checkout.resurs.com';
     public const HOSTNAME_TEST = 'omnitest.resurs.com';
 
+    /**
+     * @param string $eventName
+     * @param Request $request
+     * @return void
+     * @throws JsonException
+     * @throws CurlException
+     * @throws ValidationException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     */
     public static function registerCallback(string $eventName, Request $request): void
     {
         (new RegisterCallback())
             ->call(eventName: $eventName, request: $request);
     }
 
+    /**
+     * @param string $eventName
+     * @return Callback
+     * @throws CurlException
+     * @throws EmptyValueException
+     */
     public static function getCallback(string $eventName): Callback
     {
         return (new GetCallback())
             ->call(eventName: $eventName);
     }
 
+    /**
+     * @return CallbackCollection
+     * @throws CurlException
+     * @throws EmptyValueException
+     */
     public static function getCallbacks(): CallbackCollection
     {
         return (new GetCallbacks())
             ->call();
     }
 
+    /**
+     * @param string $eventName
+     * @return int
+     * @throws CurlException
+     * @throws EmptyValueException
+     */
     public static function deleteCallback(string $eventName): int
     {
         return (new DeleteCallback())
