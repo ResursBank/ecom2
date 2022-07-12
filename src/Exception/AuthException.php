52a49f39d054738257b502080b25b387050bcab6
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-07-12 15:19:12 +0200

    ECP-32 Change  type for AuthException

--- src/Exception/AuthException.php
+++ src/Exception/AuthException.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Resursbank\Ecom\Exception;
 
 use Exception;
+use Throwable;
 
 /**
  * Authentication exceptions.
  */
 class AuthException extends Exception
 {
+    public function __construct(string $message = "", int|string $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
