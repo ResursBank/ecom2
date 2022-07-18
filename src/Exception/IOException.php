cdf12f1a70ac331c9563c6ae9b277ca5f8592125
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-07-18 13:35:47 +0200

    ECP-44 Add StdoutLogger class

---
+++ src/Exception/IOException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Exception;
+
+use Exception;
+
+/**
+ * Specifies a general IO exception
+ */
+class IOException extends Exception
+{
+}
