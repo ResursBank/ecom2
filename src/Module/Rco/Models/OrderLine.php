ae6068ec09143ee32b3e72cced16998854f950c1
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-08-09 16:08:25 +0200

    ECP-57 Remove Init and Update-specific OrderLine classes, add more generic class. Update tests

--- src/Module/Rco/Models/OrderLine.php
+++ src/Module/Rco/Models/OrderLine.php
@@ -7,24 +7,32 @@
 
 declare(strict_types=1);
 
-namespace Resursbank\Ecom\Module\Rco\Models\InitPayment;
+namespace Resursbank\Ecom\Module\Rco\Models;
 
 use Resursbank\Ecom\Lib\Model\Model;
 
 /**
- * Defines an OrderLine object as used when creating a new payment request
+ * Defines an UpdatePayment order line object
  */
 class OrderLine extends Model
 {
+    /**
+     * @param string $artNo
+     * @param string|null $description
+     * @param float $quantity
+     * @param string $unitMeasure
+     * @param float $unitAmountWithoutVat
+     * @param float $vatPct
+     * @param string|null $type
+     */
     public function __construct(
         public string $artNo,
-        public string $description,
+        public ?string $description,
         public float $quantity,
         public string $unitMeasure,
         public float $unitAmountWithoutVat,
         public float $vatPct,
-        public ?float $totalAmountWithVat = null,
-        public ?float $totalVatAmount = null
+        public ?string $type = null
     ) {
     }
 }
