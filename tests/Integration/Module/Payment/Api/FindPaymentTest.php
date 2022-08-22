eee6d89e28ba7d9d94f13af690a6930cb97bb12f
Tomas Tornevall <tomas.tornevall@resurs.se>
2022-08-22 15:12:14 +0200

    Tests updated.

---
+++ tests/Integration/Module/Payment/Api/FindPaymentTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+
+declare(strict_types=1);
+
+namespace Resursbank\EcomTest\Integration\Module\Payment\Api;
+
+use JsonException;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Exception\AuthException;
+use Resursbank\Ecom\Exception\CurlException;
+use Resursbank\Ecom\Exception\TypeException;
+use Resursbank\Ecom\Exception\Validation\EmptyValueException;
+use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
+use Resursbank\Ecom\Exception\ValidationException;
+use Resursbank\Ecom\Lib\Cache\CacheInterface;
+use Resursbank\Ecom\Lib\Log\LoggerInterface;
+use Resursbank\Ecom\Lib\Network\Model\Auth\Jwt;
+use Resursbank\Ecom\Module\Payment\Api\FindPayment;
+
+class FindPaymentTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Config::setup(
+            logger: $this->createMock(originalClassName: LoggerInterface::class),
+            cache: $this->createMock(originalClassName: CacheInterface::class),
+            jwtAuth: new Jwt(
+                clientId: (string)$_ENV['JWT_AUTH_CLIENT_ID'],
+                clientSecret: (string)$_ENV['JWT_AUTH_CLIENT_SECRET'],
+                scope: (string)$_ENV['JWT_AUTH_SCOPE'],
+                grantType: (string)$_ENV['JWT_AUTH_GRANT_TYPE']
+            )
+        );
+    }
+
+    /**
+     * Set a store id if phpunit.xml has one (for find_payments).
+     * @return string
+     */
+    private function getStoreId()
+    {
+        return (string)($_ENV['MERCHANT_STORE_ID'] ?? '');
+    }
+
+    /**
+     * @return void
+     * @throws AuthException
+     * @throws CurlException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     * @throws JsonException
+     * @throws TypeException
+     * @throws ValidationException
+     * @throws ReflectionException
+     */
+    public function testFindPayment()
+    {
+        $reference = '20220816073146-1557096130';
+
+        (new FindPayment())->exec(
+            $this->getStoreId(),
+            $reference
+        );
+
+        $test = true;
+    }
+}
