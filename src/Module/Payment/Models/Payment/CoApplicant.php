ce43dfc1279be858d842923d27e2ffb3c6ed09c5
Tomas Tornevall <thorne@tornevall.net>
2022-08-30 13:53:29 +0200

    Missing stuff from the prior task.

--- src/Module/Payment/Models/Payment/CoApplicant.php
+++ src/Module/Payment/Models/Payment/CoApplicant.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\Payment\Models\Payment;
 
 use Resursbank\Ecom\Lib\Model\Model;
 
+/**
+ * CoApplicant for Customer models in MAPI.
+ */
 class CoApplicant extends Model
 {
     public function __construct(
