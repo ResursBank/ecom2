1ce3f46e03ca7db995edfcb43bdf5b68c3446efe
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-07 13:39:07 +0200

    ECP-94 Add class stubs for Cancel and Refund

---
+++ src/Module/Payment/Api/Cancel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Module\Payment\Api;
+
+class Cancel
+{
+}
