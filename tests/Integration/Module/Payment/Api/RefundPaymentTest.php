6b574f9da7a643101f9695bd660b0c109a306598
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-08 10:44:44 +0200

    ECP-94 Add RefundPaymentTest class stub

---
+++ tests/Integration/Module/Payment/Api/RefundPaymentTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+/** @noinspection PhpMultipleClassDeclarationsInspection */
+
+declare(strict_types=1);
+
+namespace Resursbank\EcomTest\Integration\Module\Payment\Api;
+
+use PHPUnit\Framework\TestCase;
+use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Lib\Cache\CacheInterface;
+use Resursbank\Ecom\Lib\Log\LoggerInterface;
+use Resursbank\Ecom\Lib\Network\Model\Auth\Jwt;
+
+class RefundPaymentTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Config::setup(
+            logger: $this->createMock(originalClassName: LoggerInterface::class),
+            cache: $this->createMock(originalClassName: CacheInterface::class),
+            jwtAuth: new Jwt(
+                clientId: (string)$_ENV['JWT_AUTH_CLIENT_ID'],
+                clientSecret: (string)$_ENV['JWT_AUTH_CLIENT_SECRET'],
+                scope: (string)$_ENV['JWT_AUTH_SCOPE'],
+                grantType: (string)$_ENV['JWT_AUTH_GRANT_TYPE']
+            )
+        );
+    }
+
+    public function testRefundEntirePayment(): void
+    {
+    }
+
+    public function testRefundSingleOrderLine(): void
+    {
+    }
+
+    public function testRefundWithTransactionId(): void
+    {
+    }
+
+    public function testRefundWithCreator(): void
+    {
+    }
+}
