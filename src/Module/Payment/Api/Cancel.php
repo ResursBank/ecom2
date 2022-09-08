e65142f31b8c98efc8c9ccfaaee1b79771193954
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-08 10:08:50 +0200

    ECP-94 Add working call method to Cancel

--- src/Module/Payment/Api/Cancel.php
+++ src/Module/Payment/Api/Cancel.php
@@ -10,8 +10,15 @@ declare(strict_types=1);
 namespace Resursbank\Ecom\Module\Payment\Api;
 
 use Resursbank\Ecom\Lib\Api\Mapi;
-use Resursbank\Ecom\Module\Payment\Models\OrderLineCollection;
+use Resursbank\Ecom\Lib\Network\AuthType;
+use Resursbank\Ecom\Lib\Network\ContentType;
+use Resursbank\Ecom\Lib\Network\Curl;
+use Resursbank\Ecom\Lib\Network\RequestMethod;
+use Resursbank\Ecom\Lib\Utilities\DataConverter;
+use Resursbank\Ecom\Module\Payment\Models\Order\ActionLog\OrderLineCollection;
 use Resursbank\Ecom\Module\Payment\Models\Payment;
+use stdClass;
+use Symfony\Component\Config\Definition\Exception\InvalidTypeException;
 
 /**
  * POST /payments/{payment_id}/cancel
@@ -26,12 +33,11 @@ class Cancel
         $this->mapi = new Mapi();
     }
 
-
     public function call(
+        string $orderReference,
         ?OrderLineCollection $orderLines = null,
         ?string $creator = null
-    ): Payment
-    {
+    ): Payment {
         $payload = [];
         if ($orderLines) {
             $payload['orderLines'] = $orderLines->toArray();
@@ -39,5 +45,30 @@ class Cancel
         if ($creator) {
             $payload['creator'] = $creator;
         }
+
+        $curl = new Curl(
+            url: $this->mapi->getUrl(
+                route: sprintf('%s/payments/%s/cancel', Mapi::PAYMENT_ROUTE, $orderReference)
+            ),
+            requestMethod: RequestMethod::POST,
+            payload: $payload,
+            authType: AuthType::JWT,
+            responseContentType: ContentType::JSON
+        );
+
+        $data = $curl->exec()->body;
+
+        $content = ($data instanceof stdClass) ? $data : new stdClass();
+
+        $result = DataConverter::stdClassToType(
+            object: $content,
+            type: Payment::class
+        );
+
+        if (!$result instanceof Payment) {
+            throw new InvalidTypeException(message: "Exptected Payment");
+        }
+
+        return $result;
     }
 }
