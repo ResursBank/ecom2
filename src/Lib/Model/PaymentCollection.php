d5943a75f551eccc4395e87c0eca7e990e45baac
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-19 13:10:20 +0200

    ECP-121 Moved PaymentCollection to Lib

--- src/Lib/Model/PaymentCollection.php
+++ src/Lib/Model/PaymentCollection.php
@@ -6,11 +6,10 @@
 
 declare(strict_types=1);
 
-namespace Resursbank\Ecom\Module\Payment\Models;
+namespace Resursbank\Ecom\Lib\Model;
 
 use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
 use Resursbank\Ecom\Lib\Collection\Collection;
-use Resursbank\Ecom\Lib\Model\Payment;
 
 /**
  * Defines a Payment collection.
