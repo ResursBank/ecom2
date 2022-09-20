98e7f9d79e09481b7a3168d9d3ad8879825d7f00
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-20 14:00:45 +0200

    ECP-129 Add canCapture method and tests, restructure OrderTest

--- tests/Unit/Lib/Model/Payment/OrderTest.php
+++ tests/Unit/Lib/Model/Payment/OrderTest.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace Resursbank\EcomTest\Unit\Lib\Model\Payment;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use Resursbank\Ecom\Exception\Validation\EmptyValueException;
 use Resursbank\Ecom\Exception\Validation\IllegalCharsetException;
@@ -29,12 +30,52 @@ use Resursbank\Ecom\Module\Payment\Enum\Status;
  */
 class OrderTest extends TestCase
 {
+    /**
+     * Generate a bogus UUID
+     *
+     * @throws Exception
+     */
     private function generateUuid(): string
     {
         $data = random_bytes(length: 16);
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    /**
+     * Create a dummy Payment object with the specified possible actions
+     *
+     * @param Payment\Order\PossibleActionCollection $possibleActions
+     * @return Payment
+     * @throws EmptyValueException
+     * @throws IllegalCharsetException
+     * @throws IllegalTypeException
+     * @throws IllegalValueException
+     */
+    private function createDummyPayment(Payment\Order\PossibleActionCollection $possibleActions): Payment
+    {
+        return new Payment(
+            id: $this->generateUuid(),
+            created: (new DateTime())->format(format: 'c'),
+            storeId: $this->generateUuid(),
+            paymentMethodId: $this->generateUuid(),
+            customer: new Payment\Customer(
+                customerType: CustomerType::NATURAL
+            ),
+            status: Status::ACCEPTED,
+            paymentActions: [],
+            order: new Payment\Order(
+                orderReference: $this->generateUuid(),
+                actionLog: new Payment\Order\ActionLogCollection(data: []),
+                possibleActions: $possibleActions,
+                totalOrderAmount: 100.00,
+                canceledAmount: 0.00,
+                authorizedAmount: 100.00,
+                capturedAmount: 0.00,
+                refundedAmount: 0.00
+            )
+        );
     }
 
     /**
@@ -48,52 +89,16 @@ class OrderTest extends TestCase
      */
     public function testCanCancel(): void
     {
-        $cancelable = new Payment(
-            id: $this->generateUuid(),
-            created: (new DateTime())->format(format: 'c'),
-            storeId: $this->generateUuid(),
-            paymentMethodId: $this->generateUuid(),
-            customer: new Payment\Customer(
-                customerType: CustomerType::NATURAL
-            ),
-            status: Status::ACCEPTED,
-            paymentActions: [],
-            order: new Payment\Order(
-                orderReference: $this->generateUuid(),
-                actionLog: new Payment\Order\ActionLogCollection(data: []),
-                possibleActions: new Payment\Order\PossibleActionCollection(data: [
-                    new Payment\Order\PossibleAction(action: PossibleAction::CANCEL)
-                ]),
-                totalOrderAmount: 100.00,
-                canceledAmount: 0.00,
-                authorizedAmount: 100.00,
-                capturedAmount: 0.00,
-                refundedAmount: 0.00
-            )
+        $cancelable = $this->createDummyPayment(
+            possibleActions: new Payment\Order\PossibleActionCollection(data: [
+                new Payment\Order\PossibleAction(action: PossibleAction::CANCEL)
+            ])
         );
-        $unCancelable = new Payment(
-            id: $this->generateUuid(),
-            created: (new DateTime())->format(format: 'c'),
-            storeId: $this->generateUuid(),
-            paymentMethodId: $this->generateUuid(),
-            customer: new Payment\Customer(
-                customerType: CustomerType::NATURAL
-            ),
-            status: Status::ACCEPTED,
-            paymentActions: [],
-            order: new Payment\Order(
-                orderReference: $this->generateUuid(),
-                actionLog: new Payment\Order\ActionLogCollection(data: []),
-                possibleActions: new Payment\Order\PossibleActionCollection(data: [
-                    new Payment\Order\PossibleAction(action: PossibleAction::REFUND),
-                    new Payment\Order\PossibleAction(action: PossibleAction::PARTIAL_REFUND)
-                ]),
-                totalOrderAmount: 100.00,
-                canceledAmount: 0.00,
-                authorizedAmount: 100.00,
-                capturedAmount: 0.00,
-                refundedAmount: 0.00
-            )
+        $unCancelable = $this->createDummyPayment(
+            possibleActions: new Payment\Order\PossibleActionCollection(data: [
+                new Payment\Order\PossibleAction(action: PossibleAction::REFUND),
+                new Payment\Order\PossibleAction(action: PossibleAction::PARTIAL_REFUND)
+            ])
         );
 
         $this->assertEquals(
@@ -103,6 +108,37 @@ class OrderTest extends TestCase
         $this->assertEquals(
             expected: false,
             actual: $unCancelable->canCancel()
+        );
+    }
+
+    /**
+     * Verify that the canCapture method works as intended
+     *
+     * @throws IllegalTypeException
+     * @throws EmptyValueException
+     * @throws IllegalValueException
+     * @throws IllegalCharsetException
+     */
+    public function testCanCapture(): void
+    {
+        $captureable = $this->createDummyPayment(
+            possibleActions: new Payment\Order\PossibleActionCollection(data: [
+                new Payment\Order\PossibleAction(action: PossibleAction::CAPTURE)
+            ])
+        );
+        $uncaptureable = $this->createDummyPayment(
+            possibleActions: new Payment\Order\PossibleActionCollection(data: [
+                new Payment\Order\PossibleAction(action: PossibleAction::REFUND)
+            ])
+        );
+
+        $this->assertEquals(
+            expected: true,
+            actual: $captureable->canCapture()
+        );
+        $this->assertEquals(
+            expected: false,
+            actual: $uncaptureable->canCapture()
         );
     }
 }
