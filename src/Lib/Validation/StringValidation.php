a9474fbc1a6452814d0c7cf3b7804270bfa63e11
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-09-22 09:32:27 +0200

    ECP-140 Change 0-9 to \d for inspection purposes

--- src/Lib/Validation/StringValidation.php
+++ src/Lib/Validation/StringValidation.php
@@ -195,7 +195,7 @@ class StringValidation
     {
         if (
             !preg_match(
-                pattern: '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[0-9a-d][0-9a-f]{3}-[0-9a-f]{12}$/i',
+                pattern: '/^[\da-f]{8}-[\da-f]{4}-[0-5][\da-f]{3}-[\da-d][\da-f]{3}-[\da-f]{12}$/i',
                 subject: $value
             )
         ) {
