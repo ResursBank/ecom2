dd8904b959b0a2bc6ae4e00931ee14dd4bb1fd51
Tomas Tornevall <thorne@tornevall.net>
2022-10-12 13:48:15 +0200

    Fixes by comments.

--- tests/Lib/Utilities/StringsTest.php
+++ tests/Lib/Utilities/StringsTest.php
@@ -11,18 +11,17 @@ class StringsTest extends TestCase
      * @test
      * @since 6.1.6
      */
-    public function getFullyObfuscateTest()
+    public function getObfuscatedStringTest()
     {
         $obfuscateFirst = Strings::getObfuscatedString('Just a string.', 2);
         $obfuscateSecond = Strings::getObfuscatedString('Just a string.', 3);
         $obfuscateThird = Strings::getObfuscatedString('Just a string.', 4, 0);
         // Breaking rules.
         $obfuscateFourth = Strings::getObfuscatedString('Just', 5, 5);
-        static::assertTrue(
-            $obfuscateFirst === 'Ju************.' &&
-            $obfuscateSecond === 'Jus************.' &&
-            $obfuscateThird === 'Just************' &&
-            $obfuscateFourth === 'Just'
-        );
+
+        self::assertEquals('Ju************.', $obfuscateFirst);
+        self::assertEquals('Jus************.', $obfuscateSecond);
+        self::assertEquals('Just************', $obfuscateThird);
+        self::assertEquals('Just', $obfuscateFourth);
     }
 }
