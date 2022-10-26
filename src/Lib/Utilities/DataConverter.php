9b313c6c2a0cca47ce8925500da3241b3aa4165a
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-10-26 16:04:08 +0200

    ECP-208 Add workaround for empty array data that gets converted to stdClass by json_decode

--- src/Lib/Utilities/DataConverter.php
+++ src/Lib/Utilities/DataConverter.php
@@ -18,6 +18,8 @@ use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
 use Resursbank\Ecom\Lib\Collection\Collection;
 
 use Resursbank\Ecom\Lib\Model\Model;
+use stdClass;
+
 use function call_user_func;
 use function is_object;
 
@@ -77,6 +79,13 @@ class DataConverter
                     }
                     $dummyCollection->setData(data: $converted);
                     $arguments[$name] = $dummyCollection;
+                } elseif (
+                    $propertyType === 'array' &&
+                    gettype(value: $value) === 'object' &&
+                    get_class(object: $value) === 'stdClass' &&
+                    empty((array)$value)
+                ) {
+                    $arguments[$name] = [];
                 } elseif (enum_exists($propertyType)) {
                     // If our property is an enum we need to convert the value
                     // to the enum value it represents.
