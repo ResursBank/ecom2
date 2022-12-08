30f19c65d22019b7713b862a2fe236fd8e80d0eb
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-12-08 14:32:33 +0100

    ECP-303 Add Payment\PaymentMethod model

---
+++ src/Lib/Model/Payment/PaymentMethod.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Lib\Model\Payment;
+
+use Resursbank\Ecom\Exception\Validation\EmptyValueException;
+use Resursbank\Ecom\Lib\Model\Model;
+use Resursbank\Ecom\Lib\Validation\StringValidation;
+
+/**
+ * Defines a payment method object returned when fetching a payment
+ */
+class PaymentMethod extends Model
+{
+    /**
+     * @param string $name
+     * @param StringValidation $stringValidator
+     * @throws EmptyValueException
+     */
+    public function __construct(
+        public readonly string $name,
+        private readonly StringValidation $stringValidator = new StringValidation()
+    ) {
+        $this->validateName();
+    }
+
+    /**
+     * @return void
+     * @throws EmptyValueException
+     */
+    private function validateName(): void
+    {
+        $this->stringValidator->notEmpty(value: $this->name);
+    }
+}
