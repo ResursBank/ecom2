65abe84d095b2064d1e687f7548170646219f4c0
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-01-11 14:10:55 +0100

    ECP-365 Add PropertyConverter class and some translations for error messages

---
+++ src/Lib/Network/Curl/PropertyConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Lib\Network\Curl;
+
+use Resursbank\Ecom\Exception\ConfigException;
+use Throwable;
+use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Lib\Locale\Translator;
+
+/**
+ * Converter to turn property names like "customer.mobilePhone" into more user-friendly strings like "mobile phone"
+ */
+abstract class PropertyConverter
+{
+    /**
+     * Gets converted and localized string
+     * @param string $propertyName
+     * @throws ConfigException
+     */
+    public static function get(string $propertyName): string
+    {
+        try {
+            return Translator::translate(
+                phraseId: $propertyName,
+                translationFile: __DIR__ . '/Resources/propertynames.json'
+            );
+        } catch (Throwable $error) {
+            Config::getLogger()->error(message: $error);
+            return $propertyName;
+        }
+    }
+}
