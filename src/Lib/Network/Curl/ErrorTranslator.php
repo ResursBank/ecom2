2bfd39fe8e08f26cf99ad644cd736834828b4861
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-01-11 14:15:34 +0100

    ECP-365 Use correct name for translation file

--- src/Lib/Network/Curl/ErrorTranslator.php
+++ src/Lib/Network/Curl/ErrorTranslator.php
@@ -29,7 +29,7 @@ abstract class ErrorTranslator
         try {
             return Translator::translate(
                 phraseId: $propertyName,
-                translationFile: __DIR__ . '/Resources/propertynames.json'
+                translationFile: __DIR__ . '/Resources/error.json'
             );
         } catch (Throwable $error) {
             Config::getLogger()->error(message: $error);
