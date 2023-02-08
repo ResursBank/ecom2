26cb09a50489703553a0032b5c4a49e1c9138078
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-02-08 16:07:35 +0100

    ECP-398 Add canPartiallyRefund method

--- src/Lib/Model/Payment.php
+++ src/Lib/Model/Payment.php
@@ -88,6 +88,14 @@ class Payment extends Model
     }
 
     /**
+     * Checks if payment can be partially refunded
+     */
+    public function canPartiallyRefund(): bool
+    {
+        return $this->canPerformAction(actionType: PossibleAction::PARTIAL_REFUND);
+    }
+
+    /**
      * Alias for canRefund
      */
     public function canCredit(): bool
