13b299f7d5184f6c5958b8076e79f399b89eb40b
Peter Sjöqvist <info@mageparts.com>
2023-06-07 07:52:19 +0200

    ECP-477
    * Updated get stores.

--- src/Module/Store/Widget/GetStores.php
+++ src/Module/Store/Widget/GetStores.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\Store\Widget;
 
+use JsonException;
 use Resursbank\Ecom\Exception\FilesystemException;
 use Resursbank\Ecom\Lib\Widget\Widget;
 
@@ -22,20 +23,32 @@ class GetStores extends Widget
 
     /**
      * @param string $fetchUrl Endpoint where we fetch stores.
-     * @param string $environmentSelectId ID of element containing environments.
-     * @param string $clientIdInputId ID of client id input element.
-     * @param string $clientSecretInputId ID of client secret element.
      * @param string $storeSelectId ID of element to populate with new stores.
-     * @param string $spinnerClass Class applied on store select element when fetching.
+     * @param bool $automatic
+     * @param string|null $environmentSelectId ID of element containing environments.
+     * @param string|null $clientIdInputId ID of client id input element.
+     * @param string|null $clientSecretInputId ID of client secret element.
+     * @param string|null $spinnerClass Class applied on store select element when fetching.
+     * @param string|null $clientIdInputCallback
+     * @param string|null $clientSecretInputCallback
+     * @param string|null $beforeFetchCallback
+     * @param string|null $afterFetchCallback
+     * @param string|null $fetchButtonCallback
      * @throws FilesystemException
      */
     public function __construct(
         public readonly string $fetchUrl,
-        public readonly string $environmentSelectId,
-        public readonly string $clientIdInputId,
-        public readonly string $clientSecretInputId,
-        public readonly string $storeSelectId,
-        public readonly string $spinnerClass
+        public readonly bool $automatic = true,
+        public readonly ?string $storeSelectId = null,
+        public readonly ?string $environmentSelectId = null,
+        public readonly ?string $clientIdInputId = null,
+        public readonly ?string $clientSecretInputId = null,
+        public readonly ?string $spinnerClass = null,
+        public readonly ?string $clientIdInputCallback = null,
+        public readonly ?string $clientSecretInputCallback = null,
+        public readonly ?string $beforeFetchCallback = null,
+        public readonly ?string $afterFetchCallback = null,
+        public readonly ?string $fetchButtonCallback = null
     ) {
         $this->content = $this->render(file: __DIR__ . '/get-stores.js.phtml');
     }
