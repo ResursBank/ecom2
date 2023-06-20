c77f46156db9ea98c173b5a2496c8ff6681b7dbb
Tomas Tornevall <thorne@tornevall.net>
2023-06-20 15:12:02 +0200

    ECP-523: Enum collection for Locale/Currency.

---
+++ src/Lib/Locale/Rco/Locale.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Lib\Locale\Rco;
+
+/**
+ * @codingStandardsIgnoreStart
+ */
+enum Locale: string
+{
+    case SV = 'SV';
+    case NB = 'NB';
+    case FI = 'FI';
+    case DA = 'DA';
+}
