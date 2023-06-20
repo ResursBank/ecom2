7328f4377f329c65364f48c476506b9b4fcade9f
Tomas Tornevall <thorne@tornevall.net>
2023-06-20 15:19:37 +0200

    ECP-496: Options model.

---
+++ src/Lib/Model/Rco/Payment/options.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Lib\Model\Rco\Payment;
+
+use Resursbank\Ecom\Lib\Model\Model;
+
+/**
+ * Options model for rco+ (not in pascal case). Model is lowerCased according to the docs.
+ *
+ * @SuppressWarnings(PHPMD.CamelCaseClassName)
+ */
+class options extends Model
+{
+    /**
+     * @param bool $b2bEnabled
+     * @param bool $renderCart
+     * @param bool $mutableCart
+     * @param bool $calculateShipping
+     * @param bool $lookupB2CAddress
+     * @param bool $renderCartCode
+     * @param bool $renderNotes
+     * @param array $requiredFields @todo Can we make an enum array?
+     */
+    // phpcs:ignore
+    public function __construct(
+        public readonly bool $b2bEnabled = true,
+        public readonly bool $renderCart = false,
+        public readonly bool $mutableCart = false,
+        public readonly bool $calculateShipping = false,
+        public readonly bool $lookupB2CAddress = false,
+        public readonly bool $renderCartCode = false,
+        public readonly bool $renderNotes = false,
+        public readonly array $requiredFields = ['EMAIL', 'PHONE', 'NAME', 'ADDRESS']
+    ) {
+    }
+}
