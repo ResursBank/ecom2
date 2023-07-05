0f1e0fb52df2994d377808f51bac66c8e3c98c88
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-07-05 10:37:38 +0200

    ECP-495 Use the Post class instead of the previous approach

--- src/Module/Rco/Repository.php
+++ src/Module/Rco/Repository.php
@@ -20,7 +20,7 @@ use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
 use Resursbank\Ecom\Exception\Validation\IllegalValueException;
 use Resursbank\Ecom\Exception\ValidationException;
 use Resursbank\Ecom\Lib\Model\Rco\Checkout;
-use Resursbank\Ecom\Module\Rco\Api\Init;
+use Resursbank\Ecom\Lib\Repository\Api\Rco\Post;
 
 /**
  * Main entrypoint for interfacing with the RCO+ API programmatically.
@@ -42,6 +42,30 @@ class Repository
     public static function init(
         Checkout $checkout
     ): Checkout {
-        return (new Init())->call(checkout: $checkout);
+        $result = (new Post(
+            model: Checkout::class,
+            route: 'api/checkout',
+            params: [
+                'orderReference' => $checkout->orderReference,
+                'options' => $checkout->options,
+                'locale' => $checkout->locale,
+                'currency' => $checkout->currency,
+                'cart' => $checkout->cart->toArray(),
+                'customer' => $checkout->customer,
+                'redirects' => $checkout->redirects,
+                'callbacks' => $checkout->callbacks,
+                'webhooks' => $checkout->webhooks,
+                'checkboxes' => $checkout->checkboxes->toArray(),
+                'merchant' => $checkout->merchant
+            ]
+        ))->call();
+
+        if (!$result instanceof Checkout) {
+            throw new IllegalTypeException(
+                message: 'Expected ' . Checkout::class . ', got ' . $result::class
+            );
+        }
+
+        return $result;
     }
 }
