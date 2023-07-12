efe0375d9a87d06cc5b7229714498767ab2b4832
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-07-12 07:22:52 +0200

    ECP-520 Add Rco\Repository::get method

--- src/Module/Rco/Repository.php
+++ src/Module/Rco/Repository.php
@@ -23,6 +23,7 @@ use Resursbank\Ecom\Lib\Model\Network\Header;
 use Resursbank\Ecom\Lib\Model\Rco\Checkout;
 use Resursbank\Ecom\Lib\Model\Rco\Shipping\ShippingMethodCollection;
 use Resursbank\Ecom\Lib\Repository\Api\Rco\Delete;
+use Resursbank\Ecom\Lib\Repository\Api\Rco\Get;
 use Resursbank\Ecom\Lib\Repository\Api\Rco\Patch;
 use Resursbank\Ecom\Lib\Repository\Api\Rco\Post;
 use Resursbank\Ecom\Lib\Repository\Api\Rco\Put;
@@ -278,6 +279,25 @@ class Repository
                 'orderReference' => $orderReference
             ],
             headers: $headers
+        ))->call();
+
+        if (!$response instanceof Checkout) {
+            throw new IllegalTypeException(
+                message: 'Expected ' . Checkout::class . ', got ' .
+                $response::class
+            );
+        }
+
+        return $response;
+    }
+
+    public static function get(
+        string $id
+    ): Checkout {
+        $response = (new Get(
+            model: Checkout::class,
+            route: 'api/checkout/' . $id . '/payment',
+            params: []
         ))->call();
 
         if (!$response instanceof Checkout) {
