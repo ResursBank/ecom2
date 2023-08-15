2eb278474463cb96443e0794fd5a633fec120be6
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-08-15 07:46:09 +0200

    ECP-547 Add validation of descriptions

--- src/Lib/Model/Rco/PaymentMethods/PaymentMethod.php
+++ src/Lib/Model/Rco/PaymentMethods/PaymentMethod.php
@@ -9,13 +9,23 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Lib\Model\Rco\PaymentMethods;
 
+use Resursbank\Ecom\Exception\Validation\IllegalValueException;
 use Resursbank\Ecom\Lib\Model\Model;
+use Resursbank\Ecom\Lib\Validation\ArrayValidation;
+use Resursbank\Ecom\Lib\Validation\StringValidation;
 
 /**
  * Defines a payment method.
  */
 class PaymentMethod extends Model
 {
+    /**
+     * @param array|null $required
+     * @param array|null $descriptions
+     * @param array|null $links
+     * @throws IllegalValueException
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
     public function __construct(
         public readonly ?string $methodId = null,
         public readonly ?string $name = null,
@@ -25,7 +35,36 @@ class PaymentMethod extends Model
         public readonly ?string $subtitle = null,
         public readonly ?array $descriptions = null,
         public readonly ?string $terms = null,
-        public readonly ?array $links = null
+        public readonly ?array $links = null,
+        private readonly ArrayValidation $arrayValidation = new ArrayValidation(),
+        private readonly StringValidation $stringValidation = new StringValidation()
     ) {
+        $this->validateDescriptions();
+    }
+
+    /**
+     * Validate descriptions.
+     *
+     * @throws IllegalValueException
+     */
+    private function validateDescriptions(): void
+    {
+        if (!$this->descriptions) {
+            return;
+        }
+
+        $this->arrayValidation->length(
+            data: $this->descriptions,
+            min: 1,
+            max: 10000
+        );
+
+        foreach ($this->descriptions as $description) {
+            $this->stringValidation->length(
+                value: $description,
+                min: 1,
+                max: 10000
+            );
+        }
     }
 }
