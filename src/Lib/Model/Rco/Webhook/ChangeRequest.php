9a1d800694f4582f21584e37282363775aab4ea5
Peter Sjöqvist <info@mageparts.com>
2023-09-20 10:54:20 +0200

    ECP-629
    * Corrections based on answers from RCO dev team.

--- src/Lib/Model/Rco/Webhook/ChangeRequest.php
+++ src/Lib/Model/Rco/Webhook/ChangeRequest.php
@@ -11,16 +11,19 @@ namespace Resursbank\Ecom\Lib\Model\Rco\Webhook;
 
 use Resursbank\Ecom\Lib\Model\Model;
 use Resursbank\Ecom\Lib\Model\Rco\CreateCart\ItemCollection;
+use Resursbank\Ecom\Lib\Model\Rco\CreateShippingMethodCollection;
+use Resursbank\Ecom\Lib\Model\Rco\Recipient;
 
 /**
- * Implementation of WebhookResponseDto object.
+ * Implementation of WebhookChangeRequestDto object.
  */
-class Response extends Model
+class ChangeRequest extends Model
 {
     public function __construct(
         public readonly ?Recipient $delivery,
         public readonly ?Recipient $billing,
         public readonly ?CreateShippingMethodCollection $shippingMethods,
+        public readonly ?string $cartCode,
         public readonly ?string $orderReference,
         public readonly ?ItemCollection $cartItems
     ) {
