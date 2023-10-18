9e5d41e3ebda148b9b193f8b69db1d521328fd91
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-10-18 14:49:09 +0200

    ECP-617 Add simple StringMatchesUrl validation support

--- src/Lib/Attribute/Validation/StringMatchesUrl.php
+++ src/Lib/Attribute/Validation/StringMatchesUrl.php
@@ -10,7 +10,11 @@ declare(strict_types=1);
 namespace Resursbank\Ecom\Lib\Attribute\Validation;
 
 use Attribute;
+use Exception;
+use ReflectionParameter;
 use Resursbank\Ecom\Exception\Validation\IllegalUrlException;
+use Resursbank\Ecom\Lib\Attribute\Validation\Interface\StringInterface;
+use Resursbank\Ecom\Lib\Utilities\Random;
 
 use function preg_match;
 
@@ -18,7 +22,7 @@ use function preg_match;
  * Used for regex validation of urls.
  */
 #[Attribute(flags: Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
-class StringMatchesUrl
+class StringMatchesUrl implements StringInterface
 {
     /**
      * @throws IllegalUrlException
@@ -35,5 +39,44 @@ class StringMatchesUrl
                 message: $name . ' value ' . $value . ' is not an URL'
             );
         }
+    }
+
+    /**
+     * @inheritDoc
+     * @param ReflectionParameter $parameter
+     * @param int $size
+     * @return array
+     * @throws Exception
+     */
+    public function getAcceptedValues(ReflectionParameter $parameter, int $size = 5): array
+    {
+        $urls = [];
+        $characters = range(start: 'a', end: 'z');
+
+        for ($i = 0; $i < $size; $i++) {
+            $urls[] = 'https://' .
+                Random::getString(length: 12, characters: $characters) . '.' .
+                Random::getString(length: 3, characters: $characters);
+        }
+
+        return $urls;
+    }
+
+    /**
+     * @inheritDoc
+     * @param ReflectionParameter $parameter
+     * @param int $size
+     * @return array
+     * @throws Exception
+     */
+    public function getRejectedValues(ReflectionParameter $parameter, int $size = 5): array
+    {
+        $urls = [];
+
+        for ($i = 0; $i < $size; $i++) {
+            $urls[] = 'hppt:/' . Random::getString(length: 24);
+        }
+
+        return $urls;
     }
 }
