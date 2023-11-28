7b47aee46b5a40a9e2ff5711bd08521c777a09d9
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-11-28 15:44:50 +0100

    ECP-544 Remove URL validation on Cart\Item

--- src/Lib/Model/Rco/Cart/Item.php
+++ src/Lib/Model/Rco/Cart/Item.php
@@ -12,7 +12,6 @@ namespace Resursbank\Ecom\Lib\Model\Rco\Cart;
 use Resursbank\Ecom\Lib\Attribute\Validation\ArrayOfStrings;
 use Resursbank\Ecom\Lib\Attribute\Validation\IntValue;
 use Resursbank\Ecom\Lib\Attribute\Validation\StringLength;
-use Resursbank\Ecom\Lib\Attribute\Validation\StringMatchesUrl;
 use Resursbank\Ecom\Lib\Model\Model;
 use Resursbank\Ecom\Lib\Model\Rco\Enum\CartItemType;
 
@@ -26,7 +25,7 @@ class Item extends Model
      */
     public function __construct(
         public readonly string $description,
-        #[StringMatchesUrl] public readonly string $imageUrl,
+        public readonly string $imageUrl,
         #[StringLength(min: 1, max: 36)] public readonly string $itemId,
         #[StringLength(min: 1, max: 36)] public readonly string $itemIdDisplay,
         #[IntValue(max: (2 ** 31) - 1)] public readonly int $maxQuantity,
@@ -40,7 +39,7 @@ class Item extends Model
         #[IntValue(max: (2 ** 31) - 1)] public readonly int $totalTax,
         public readonly CartItemType $type,
         #[IntValue(max: (2 ** 31) - 1)] public readonly int $unitPrice,
-        #[StringMatchesUrl] public readonly string $url
+        public readonly string $url
     ) {
         parent::__construct();
     }
