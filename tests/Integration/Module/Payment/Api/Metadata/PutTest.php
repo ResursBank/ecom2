ce9076e05d37b442c3f6645a8649938edf4fc5b9
Peter Sjöqvist <peter.sjoqvist@resurs.se>
2024-03-18 08:14:11 +0100

    Updates

--- tests/Integration/Module/Payment/Api/Metadata/PutTest.php
+++ tests/Integration/Module/Payment/Api/Metadata/PutTest.php
@@ -126,7 +126,7 @@ class PutTest extends TestCase
                 ),
                 customerType: CustomerType::NATURAL,
                 contactPerson: 'Vincent',
-                email: 'test@hosted.resurs',
+                email: 'test@resurs.com',
                 governmentId: '198305147715',
                 mobilePhone: '46701234567',
                 deviceInfo: new DeviceInfo()
