a06614159dcb45638f41ae46d344c68cedd235c5
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-03-18 14:03:59 +0100

    ECP-636 Payment\Api\Capture from RCO+

--- src/Module/Payment/Api/Capture.php
+++ src/Module/Payment/Api/Capture.php
@@ -27,7 +27,6 @@ use Resursbank\Ecom\Lib\Network\ContentType;
 use Resursbank\Ecom\Lib\Network\Curl;
 use Resursbank\Ecom\Lib\Network\RequestMethod;
 use Resursbank\Ecom\Lib\Utilities\DataConverter;
-use stdClass;
 
 /**
  * POST /payments/{payment_id}/capture
@@ -96,12 +95,8 @@ class Capture
             forceObject: empty($payload)
         );
 
-        $data = $curl->exec()->body;
-
-        $content = $data instanceof stdClass ? $data : new stdClass();
-
         $result = DataConverter::stdClassToType(
-            object: $content,
+            object: $curl->exec()->body,
             type: Payment::class
         );
 
