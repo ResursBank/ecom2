45729bea820b7763fb8c2bb64f2a449e80010cb9
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-03-19 10:21:08 +0100

    ECP-636 phpcbf fixes

--- tests/Integration/Lib/Log/FileLoggerTest.php
+++ tests/Integration/Lib/Log/FileLoggerTest.php
@@ -384,8 +384,8 @@ final class FileLoggerTest extends TestCase
     public function testValidatePathWhichIsNotDirectory(): void
     {
         $filePath = $this->path . DIRECTORY_SEPARATOR . bin2hex(
-                string: random_bytes(length: 8)
-            );
+            string: random_bytes(length: 8)
+        );
 
         if (!touch(filename: $filePath)) {
             $this::markTestSkipped(message: 'Failed to create file for test');
