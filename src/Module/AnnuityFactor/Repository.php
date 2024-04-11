70e085e80ff96547a35d4652c474c6d7bc79f753
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-04-11 10:28:50 +0200

    ECP-735 Remove unnecesary GET parameters

--- src/Module/AnnuityFactor/Repository.php
+++ src/Module/AnnuityFactor/Repository.php
@@ -159,7 +159,8 @@ class Repository
         return new Get(
             model: AnnuityFactors::class,
             route: Mapi::STORE_ROUTE . "/$storeId/payment_methods/$paymentMethodId/annuity_factors",
-            params: compact('storeId', 'paymentMethodId')
+            //params: compact('storeId', 'paymentMethodId')
+            params: []
         );
     }
 
