9da3a32e72101b43ce290ddb8366cbda0c2035b0
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-05-06 14:33:53 +0200

    ECP-742 Add unit tests

--- src/Module/Callback/Widget/Callback.php
+++ src/Module/Callback/Widget/Callback.php
@@ -9,35 +9,44 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\Callback\Widget;
 
+use Resursbank\Ecom\Exception\FilesystemException;
 use Resursbank\Ecom\Lib\Locale\Translator;
 use Resursbank\Ecom\Lib\Widget\Widget;
 
+/**
+ * Callback URL list widget.
+ */
 class Callback extends Widget
 {
+    /** @var string */
     public readonly string $content;
 
     public function __construct(
         private readonly ?string $authorizationUrl = null,
         private readonly ?string $managementUrl = null
-    )
-    {
+    ) {
         $this->renderWidget();
     }
 
     public function getAuthorizationUrl(): ?string
     {
-        return $this->authorizationUrl;
+        return $this->authorizationUrl ??
+            Translator::translate(phraseId: 'failed-to-resolve-callback-url');
     }
 
     public function getManagementUrl(): ?string
     {
-        return $this->managementUrl;
+        return $this->managementUrl ??
+            Translator::translate(phraseId: 'failed-to-resolve-callback-url');
     }
 
+    /**
+     * Render widget content.
+     *
+     * @throws FilesystemException
+     */
     protected function renderWidget(): void
     {
-        $this->content = $this->render(
-            file: __DIR__ . '/callback.phtml'
-        );
+        $this->content = $this->render(file: __DIR__ . '/callback.phtml');
     }
 }
