e5bf27f557cd4fa1e8f80540bb6c9c49dda7c740
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-05-28 12:15:46 +0200

    Fix.

--- src/Lib/Attribute/Validation/StringIsIpAddress.php
+++ src/Lib/Attribute/Validation/StringIsIpAddress.php
@@ -32,8 +32,7 @@ class StringIsIpAddress extends StringValidation
      */
     public function validate(string $name, ?string $value = null): void
     {
-        if (
-            $value !== null &&
+        if ($value !== null &&
             !filter_var(value: $value, filter: FILTER_VALIDATE_IP)
         ) {
             throw new IllegalIpException(
