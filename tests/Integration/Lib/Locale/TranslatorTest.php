bad25f84d7831bb7adec8d723182a8b74d3dc7ad
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-05-30 08:19:21 +0200

    ...

--- tests/Integration/Lib/Locale/TranslatorTest.php
+++ tests/Integration/Lib/Locale/TranslatorTest.php
@@ -37,10 +37,6 @@ class TranslatorTest extends TestCase
         parent::setUp();
     }
 
-    /**
-     * @param Language $locale
-     * @return void
-     */
     private function setupConfig(Language $locale = Language::EN): void
     {
         Config::setup(
