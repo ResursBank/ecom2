89f8794577260f1e2a404fe74b5d276b8c953f46
Peter Sjöqvist <peter.sjoqvist@resurs.se>
2024-07-11 13:24:15 +0200

    ECP-734

--- src/Module/AnnuityFactor/Widget/GetPeriods.php
+++ src/Module/AnnuityFactor/Widget/GetPeriods.php
@@ -9,20 +9,28 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\AnnuityFactor\Widget;
 
+use JsonException;
+use ReflectionException;
 use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Exception\ApiException;
+use Resursbank\Ecom\Exception\AuthException;
+use Resursbank\Ecom\Exception\CacheException;
 use Resursbank\Ecom\Exception\ConfigException;
+use Resursbank\Ecom\Exception\CurlException;
 use Resursbank\Ecom\Exception\FilesystemException;
+use Resursbank\Ecom\Exception\Validation\EmptyValueException;
+use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
+use Resursbank\Ecom\Exception\Validation\IllegalValueException;
+use Resursbank\Ecom\Exception\ValidationException;
 use Resursbank\Ecom\Lib\Model\AnnuityFactor\AnnuityInformation;
-use Resursbank\Ecom\Lib\Model\AnnuityFactor\AnnuityInformationCollection;
 use Resursbank\Ecom\Lib\Model\PaymentMethod;
-use Resursbank\Ecom\Lib\Model\PaymentMethodCollection;
 use Resursbank\Ecom\Lib\Widget\Widget;
 use Resursbank\Ecom\Module\AnnuityFactor\Repository;
 use Resursbank\Ecom\Module\PaymentMethod\Repository as PaymentMethodRepository;
 use Throwable;
 
 /**
- * Render JavaScript code to fetch list of periods.
+ * Render JavaScript code to sync list of periods with selected payment method.
  */
 class GetPeriods extends Widget
 {
@@ -47,15 +55,26 @@ class GetPeriods extends Widget
     }
 
     /**
-     * Resolve list of periods, sectioned by payment method.
+     * Fetch annuity factors for each payment method and add them to the
+     * resulting array. Each payment method defines an inner array with the
+     * annuity factors for that payment method, keyed by the period.
      */
     public function getJsonData(): string
     {
         try {
-            return json_encode(
-                value: $this->getPeriods(),
-                flags: JSON_THROW_ON_ERROR
+            $result = [];
+            $methods = PaymentMethodRepository::getPaymentMethods(
+                storeId: $this->storeId
             );
+
+            /** @var PaymentMethod $method */
+            foreach ($methods as $method) {
+                $result[$method->getId()] = $this->getAnnuityFactorsForMethod(
+                    $method
+                );
+            }
+
+            return json_encode(value: $result, flags: JSON_THROW_ON_ERROR);
         } catch (Throwable $error) {
             try {
                 Config::getLogger()->error($error);
@@ -68,66 +87,28 @@ class GetPeriods extends Widget
     }
 
     /**
-     * Fetch annuity factors for each payment method and add them to the result
-     * array. Each payment method defines an inner array with the annuity
-     * factors for that payment method, keyed by the period.
-     *
-     * @return array
-     */
-    private function getPeriods(): array
-    {
-        $methods = $this->getPaymentMethods();
-
-        if ($methods === null) {
-            return [];
-        }
-
-        return $this->getAnnuityFactorsForMethods($methods);
-    }
-
-    /**
-     * Resolve annuity factors from a collection of payment methods.
-     */
-    private function getAnnuityFactorsForMethods(
-        PaymentMethodCollection $methods
-    ): array {
-        $result = [];
-
-        /** @var PaymentMethod $method */
-        foreach ($methods as $method) {
-            $annuityFactors = $this->getAnnuityFactorsForMethod($method);
-
-            if ($annuityFactors === null) {
-                continue;
-            }
-
-            $result[$method->getId()] = $annuityFactors;
-        }
-
-        return $result;
-    }
-
-    /**
      * Resolve annuity factors for a specific payment method.
+     *
+     * @throws ApiException
+     * @throws AuthException
+     * @throws CacheException
+     * @throws ConfigException
+     * @throws CurlException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     * @throws IllegalValueException
+     * @throws JsonException
+     * @throws ReflectionException
+     * @throws Throwable
+     * @throws ValidationException
      */
-    private function getAnnuityFactorsForMethod(PaymentMethod $method): ?array
+    private function getAnnuityFactorsForMethod(PaymentMethod $method): array
     {
-        $annuityFactors = $this->getAnnuityFactors($this->storeId, $method);
-
-        if ($annuityFactors === null) {
-            return null;
-        }
-
-        return $this->getAnnuityFactorsArray($annuityFactors);
-    }
-
-    /**
-     * Resolve annuity factors as an array.
-     */
-    private function getAnnuityFactorsArray(
-        AnnuityInformationCollection $annuityFactors
-    ): array {
         $result = [];
+        $annuityFactors = Repository::getAnnuityFactors(
+            storeId: $this->storeId,
+            paymentMethodId: $method->getId()
+        );
 
         /** @var AnnuityInformation $annuityFactor */
         foreach ($annuityFactors as $annuityFactor) {
@@ -135,50 +116,5 @@ class GetPeriods extends Widget
         }
 
         return $result;
-    }
-
-    /**
-     * Resolve list of payment methods.
-     */
-    private function getPaymentMethods(): ?PaymentMethodCollection
-    {
-        try {
-            return PaymentMethodRepository::getPaymentMethods(
-                storeId: $this->storeId
-            );
-        } catch (Throwable $error) {
-            try {
-                Config::getLogger()->error($error);
-            } catch (ConfigException) {
-                // Do nothing.
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Get annuity factors for a specific payment method.
-     */
-    private function getAnnuityFactors(
-        string $storeId,
-        PaymentMethod $method
-    ): ?AnnuityInformationCollection {
-        try {
-            $result = Repository::getAnnuityFactors(
-                storeId: $storeId,
-                paymentMethodId: $method->getId()
-            );
-
-            return count($result) > 0 ? $result : null;
-        } catch (Throwable $error) {
-            try {
-                Config::getLogger()->error($error);
-            } catch (ConfigException) {
-                // Do nothing.
-            }
-        }
-
-        return null;
     }
 }
--- src/Module/PaymentMethod/Http/PartPayment/InfoControllerInterface.php
+++ src/Module/PaymentMethod/Http/PartPayment/InfoControllerInterface.php
@@ -5,8 +5,6 @@
  * See LICENSE for license details.
  */
 
-/** @noinspection PhpMultipleClassDeclarationsInspection */
-
 declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\PaymentMethod\Http\PartPayment;
