c30070b8df629a468f7f69ce2043b35620b3c51e
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-09-09 08:38:06 +0200

    ECP-841 Remove unused imports

--- src/Lib/Attribute/Validation/StringMatchesRegex.php
+++ src/Lib/Attribute/Validation/StringMatchesRegex.php
@@ -10,16 +10,9 @@ declare(strict_types=1);
 namespace Resursbank\Ecom\Lib\Attribute\Validation;
 
 use Attribute;
-use JsonException;
-use ReflectionException;
-use Resursbank\Ecom\Exception\ConfigException;
-use Resursbank\Ecom\Exception\FilesystemException;
-use Resursbank\Ecom\Exception\TranslationException;
 use Resursbank\Ecom\Exception\Validation\IllegalCharsetException;
-use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
 use Resursbank\Ecom\Exception\Validation\IllegalValueException;
 use Resursbank\Ecom\Lib\Attribute\Validation\Traits\TranslatifyPropertyName;
-use Resursbank\Ecom\Lib\Locale\Translator;
 
 use function preg_match;
 
