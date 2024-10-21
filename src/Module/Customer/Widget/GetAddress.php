7a461c9bf7ff174e1da144deae008043eb52ac0d
Mikael Jacobson <mikael.jacobson@resurs.se>
2024-10-21 10:21:30 +0200

    ECP-905 Add shouldRender check

--- src/Module/Customer/Widget/GetAddress.php
+++ src/Module/Customer/Widget/GetAddress.php
@@ -9,9 +9,14 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Module\Customer\Widget;
 
+use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Exception\ConfigException;
 use Resursbank\Ecom\Exception\FilesystemException;
 use Resursbank\Ecom\Lib\Order\CustomerType;
 use Resursbank\Ecom\Lib\Widget\Widget;
+use Resursbank\Ecom\Module\Store\Enum\Country;
+use Resursbank\Ecom\Module\Store\Repository as StoreRepository;
+use Throwable;
 
 /**
  * Read more widget.
@@ -34,9 +39,10 @@ class GetAddress extends Widget
     public readonly string $js;
 
     /**
+     * @throws ConfigException
      * @throws FilesystemException
      * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
-     * @SuppressWarnings(PHPMD.LongVariable)
+     * @SuppressWarnings(PHPMD.ElseExpression)
      */
     public function __construct(
         public readonly string $url = '',
@@ -44,8 +50,43 @@ class GetAddress extends Widget
         public CustomerType $customerType = CustomerType::NATURAL,
         public readonly bool $automatic = false
     ) {
-        $this->content = $this->render(file: __DIR__ . '/get-address.phtml');
-        $this->css = $this->render(file: __DIR__ . '/get-address.css');
-        $this->js = $this->render(file: __DIR__ . '/get-address.js.phtml');
+        if ($this->shouldRender()) {
+            $this->content = $this->render(
+                file: __DIR__ . '/get-address.phtml'
+            );
+            $this->css = $this->render(file: __DIR__ . '/get-address.css');
+            $this->js = $this->render(file: __DIR__ . '/get-address.js.phtml');
+        } else {
+            $this->content = '';
+            $this->css = '';
+            $this->js = '';
+        }
+    }
+
+    /**
+     * Check if widget should be rendered.
+     *
+     * The widget should only render for stores in Sweden.
+     *
+     * @throws ConfigException
+     */
+    private function shouldRender(): bool
+    {
+        try {
+            $stores = StoreRepository::getStores(size: 1);
+
+            if (Config::getStoreId() !== null) {
+                $store = $stores->filterById(id: Config::getStoreId());
+
+                if ($store !== null && $store->countryCode === Country::SE) {
+                    return true;
+                }
+            }
+        } catch (Throwable $error) {
+            Config::getLogger()->error(message: $error);
+            return false;
+        }
+
+        return false;
     }
 }
