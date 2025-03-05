b4089af8c864c248c2f7a394ca32a48da447e588
Mikael Jacobson <mikael.jacobson@resurs.se>
2025-03-05 07:55:50 +0100

    ECP-936 QA fix

--- src/Lib/Repository/Traits/ModelConverter.php
+++ src/Lib/Repository/Traits/ModelConverter.php
@@ -17,7 +17,6 @@ use Resursbank\Ecom\Exception\Validation\IllegalTypeException;
 use Resursbank\Ecom\Exception\Validation\IllegalValueException;
 use Resursbank\Ecom\Lib\Collection\Collection;
 use Resursbank\Ecom\Lib\Model\Model;
-use Resursbank\Ecom\Lib\Model\PriceSignage\PriceSignage;
 use Resursbank\Ecom\Lib\Utilities\DataConverter;
 use stdClass;
 
