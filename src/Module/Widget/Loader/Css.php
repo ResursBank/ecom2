6904d774334667fa435b8ccc9056e9822d2fd959
Peter Sjöqvist <peter.sjoqvist@resurs.se>
2025-08-29 13:47:19 +0200

    ECP-1072 Completed widget

---
+++ src/Module/Widget/Loader/Css.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Module\Widget\Loader;
+
+use Resursbank\Ecom\Lib\Widget\Widget;
+
+/**
+ * Renders Loader CSS for use in admin panel order view
+ */
+class Css extends Widget
+{
+    public readonly string $content;
+
+    public function __construct()
+    {
+        $this->content = $this->renderStatic(
+            file: __DIR__ . DIRECTORY_SEPARATOR . 'templates' .
+            DIRECTORY_SEPARATOR . 'css.css'
+        );
+    }
+}
