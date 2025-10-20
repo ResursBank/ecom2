d88c0bbd58f1d95998fc6f6511543d7298db939b
Mikael Jacobson <mikael.jacobson@resurs.se>
2025-10-20 08:09:28 +0200

    PD-2621 Initial commit of Test Purche widget stubs

---
+++ src/Module/Widget/TestPurchase/Css.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Module\Widget\TestPurchase;
+
+use Resursbank\Ecom\Lib\Widget\Widget;
+
+class Css extends Widget
+{
+}
---
+++ src/Module/Widget/TestPurchase/Html.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Ecom\Module\Widget\TestPurchase;
+
+use Resursbank\Ecom\Lib\Widget\Widget;
+
+class Html extends Widget
+{
+    public readonly string $content;
+
+    public function __construct()
+    {
+    }
+}
