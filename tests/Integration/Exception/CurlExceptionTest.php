5e60567483e4b1a111825a3a438aa0e5dc73569e
Mikael Jacobson <mikael.jacobson@resurs.se>
2026-01-12 07:36:51 +0100

    PD-3928 Fix CurlExeptionTest, move and fix TestPurchase HTML widget test. Fix inspections for TestPurchase HTML widget.

--- tests/Integration/Exception/CurlExceptionTest.php
+++ tests/Integration/Exception/CurlExceptionTest.php
@@ -26,9 +26,9 @@ use Resursbank\Ecom\Lib\Model\Payment\Order\ActionLog\OrderLineCollection;
 use Resursbank\Ecom\Lib\Order\CountryCode;
 use Resursbank\Ecom\Lib\Order\CustomerType;
 use Resursbank\Ecom\Lib\Order\OrderLineType;
+use Resursbank\Ecom\Lib\Utilities\MockSigner;
 use Resursbank\Ecom\Lib\Utilities\Strings;
 use Resursbank\Ecom\Module\Payment\Repository;
-use Resursbank\EcomTest\Utilities\MockSigner;
 
 /**
  * Verifies that the CurlException class works as intended when resolving
--- tests/Integration/Module/Widget/TestPurchase/HtmlTest.php
+++ tests/Integration/Module/Widget/TestPurchase/HtmlTest.php
@@ -7,10 +7,12 @@
 
 declare(strict_types=1);
 
-namespace Resursbank\EcomTest\Unit\Module\Widget\TestPurchase;
+namespace Resursbank\EcomTest\Integration\Module\Widget\TestPurchase;
 
 use PHPUnit\Framework\TestCase;
 use Resursbank\Ecom\Config;
+use Resursbank\Ecom\Lib\Api\GrantType;
+use Resursbank\Ecom\Lib\Model\Network\Auth\Jwt;
 use Resursbank\Ecom\Module\Widget\TestPurchase\Html;
 
 /**
@@ -23,7 +25,14 @@ class HtmlTest extends TestCase
      */
     protected function setUp(): void
     {
-        Config::setup();
+        Config::setup(
+            jwtAuth: new Jwt(
+                clientId: $_ENV['JWT_AUTH_CLIENT_ID'],
+                clientSecret: $_ENV['JWT_AUTH_CLIENT_SECRET'],
+                grantType: GrantType::from(value: $_ENV['JWT_AUTH_GRANT_TYPE'])
+            ),
+            storeId: $_ENV['STORE_ID']
+        );
     }
 
     /**
