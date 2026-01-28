a8c86af66a7af283113668dc6872d13470f542d9
Peter Sjöqvist <peter.sjoqvist@resurs.se>
2026-01-28 10:01:23 +0100

    ECP-3949 Changed API url for PWS

--- src/Lib/Api/Rws.php
+++ src/Lib/Api/Rws.php
@@ -17,7 +17,7 @@ use Resursbank\Ecom\Lib\Validation\StringValidation;
  */
 class Rws
 {
-    public const URL_TEST = 'https://apigw.integration.resurs.com/api/mock_payment_widget_service/';
+    public const URL_TEST = 'https://web-integration-mock-payment-widget-service.wa3.int.resurs.cloud/api/';
     public const ROUTE_SESSION = 'session';
     public const PAYMENT_METHODS_ROUTE = 'payment-methods';
 
