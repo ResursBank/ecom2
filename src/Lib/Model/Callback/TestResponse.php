631efa58bffb4f413aa3018eab2bc84c8c0718e5
Mikael Jacobson <mikael.jacobson@resurs.se>
2026-03-26 14:55:59 +0100

    PD-2854 Use attribute validation for Lib\Model\Callback\TestResponse.

--- src/Lib/Model/Callback/TestResponse.php
+++ src/Lib/Model/Callback/TestResponse.php
@@ -9,6 +9,10 @@ declare(strict_types=1);
 
 namespace Resursbank\Ecom\Lib\Model\Callback;
 
+use JsonException;
+use ReflectionException;
+use Resursbank\Ecom\Exception\AttributeCombinationException;
+use Resursbank\Ecom\Lib\Attribute\Validation\IntValue;
 use Resursbank\Ecom\Lib\Model\Callback\Enum\TestStatus;
 use Resursbank\Ecom\Lib\Model\Model;
 
@@ -18,11 +22,14 @@ use Resursbank\Ecom\Lib\Model\Model;
 class TestResponse extends Model
 {
     /**
-     * @param int $code HTTP code.
+     * @throws JsonException
+     * @throws ReflectionException
+     * @throws AttributeCombinationException
      */
     public function __construct(
         public readonly TestStatus $status,
-        public readonly int $code
+        #[IntValue(min:100, max:599)] public readonly int $code
     ) {
+        parent::__construct();
     }
 }
