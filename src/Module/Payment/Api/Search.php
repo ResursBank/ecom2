a887bfea605d0f1ab65c2b3cfd30e968ecb2e947
Mikael Jacobson <mikael.jacobson@resurs.se>
2026-05-05 10:02:58 +0200

    PD-2185 Remove todo comment.

--- src/Module/Payment/Api/Search.php
+++ src/Module/Payment/Api/Search.php
@@ -62,7 +62,6 @@ class Search
      * @throws ValidationException
      * @throws AttributeCombinationException
      * @throws NotJsonEncodedException
-     * @todo Refactor ECP-357. Remove phpcs:ignore when done.
      */
     public function call(
         ?string $orderReference = null,
