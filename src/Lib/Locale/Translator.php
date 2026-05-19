a768564f6d7ffc2a0e087f9ae75857c31b15cab6
Mikael Jacobson <mikael.jacobson@resurs.se>
2026-05-19 07:58:36 +0200

    PD-2505 Use sha1 hash for all translation cache keys to keep cache key length down.

--- src/Lib/Locale/Translator.php
+++ src/Lib/Locale/Translator.php
@@ -147,18 +147,16 @@ abstract class Translator
 
     /**
      * Generates a valid cache key which includes the name of the translation file.
-     *
-     * @todo preg_replace returns null|array|string, this method is required to return string ECP-372
      */
     public static function getCacheKey(?string $translationFile = null): string
     {
         $rawKey = ($translationFile ?
-            (self::$cacheKey . '-' . $translationFile) :
-            (self::$cacheKey . '-' . preg_replace(
+            (self::$cacheKey . '-' . sha1(string: $translationFile)) :
+            (self::$cacheKey . '-' . sha1(string: preg_replace(
                 pattern: '/[0-9]/',
                 replacement: '',
                 subject: sha1(string: self::$translationsFilePath)
-            ))
+            ) ?? ''))
         );
         $key = preg_replace(
             pattern: '/[^a-zA-Z\d\-_]/',
