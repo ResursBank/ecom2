f40b189f32ec110057fca530d8fc77b5af6085a6
Mikael Jacobson <mikael.jacobson@resurs.se>
2026-05-20 09:35:10 +0200

    PD-2379 Remove old todo.

--- src/Lib/Log/LogLevel.php
+++ src/Lib/Log/LogLevel.php
@@ -24,11 +24,9 @@ enum LogLevel: int
     case EXCEPTION = 4;
 
     /**
-     * Checks if supplied log level should be logged according to current configured logLevel.
+     * Check if configured log level is less than or equal to supplied level.
      *
-     * @param LogLevel $level
      * @throws ConfigException
-     * @todo Check if ConfigException validation needs a test.
      */
     public static function loggable(self $level): bool
     {
